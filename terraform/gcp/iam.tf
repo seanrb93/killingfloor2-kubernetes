3daa17574c38b03acfaa67033e1d992ed4fad722
Sean Brankin <SeanBrankin@mail.com>
2025-12-12 15:16:49 +0000

    Reintroduce GKE load balancer admin IAM member resource in main.tf

--- terraform/gcp/iam.tf
+++
@@ -1,6 +0,0 @@
-resource "google_project_iam_member" "gke_lb_admin" {
-  project = var.project_id
-  role    = "roles/compute.loadBalancerAdmin"
-
-  member = "serviceAccount:service-${data.google_project.this.number}@container-engine-robot.iam.gserviceaccount.com"
-}
--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -25,6 +25,13 @@ provider "google" {
   zone    = var.zone
 }
 
+resource "google_project_iam_member" "gke_lb_admin" {
+  project = var.project_id
+  role    = "roles/compute.loadBalancerAdmin"
+
+  member = "serviceAccount:service-${data.google_project.this.number}@container-engine-robot.iam.gserviceaccount.com"
+}
+
 resource "google_project_service" "container" {
   project = var.project_id
   service = "container.googleapis.com"
